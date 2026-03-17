e4ab78b5898fbd76e099a413a32a6e5be4cb38de
Jakub Burakowski <jakub.burakowski@circlekeurope.com>
2026-03-17 10:12:30 +0100

    init

---
+++ src/test/java/com/misiakidebesciaki/szkolenie/SzkolenieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misiakidebesciaki.szkolenie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzkolenieApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
